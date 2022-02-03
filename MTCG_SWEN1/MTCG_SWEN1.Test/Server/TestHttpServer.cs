6295b5c7e90408c3bdf3823f355313a89b5851b2
yqni13 <65848806+yqni13@users.noreply.github.com>
2022-02-03 20:06:52 +0100

    first unit-test (not working right now because of single thread)

---
+++ MTCG_SWEN1/MTCG_SWEN1.Test/Server/TestHttpServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MTCG_SWEN1.Server;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace MTCG_SWEN1.Test.Server
+{
+    [TestFixture]
+    class TestHttpServer
+    {
+        // Arrange
+        private IPGlobalProperties _properties = IPGlobalProperties.GetIPGlobalProperties();
+        private IPAddress _localAddr = IPAddress.Parse("127.0.0.1");
+        private int _port = 10001;
+
+        [Test]
+        public void TestHttpServer_ConnectExistingSocket()
+        {
+            IPEndPoint[] endPoints = _properties.GetActiveTcpListeners();
+
+            foreach (IPEndPoint e in endPoints)
+            {
+                if(e.ToString() == $"{_localAddr}:{_port}")
+                {
+                    Assert.IsTrue(false, "port existing");
+                }                
+            }
+            Assert.IsTrue(true, "port not existing");
+        }
+    }
+}
