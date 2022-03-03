e6b841d5e1818a4815df1c1935f6567dd262d3b6
yqni13 <65848806+yqni13@users.noreply.github.com>
2022-03-03 18:20:30 +0100

    added sql script
    + deleted unnecessary classes

---
+++ MTCG_SWEN1/MTCG_SWEN1/DB/DataBaseConnection.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG_SWEN1.DB
+{
+    class DataBaseConnection
+    {
+        public static IDbConnection Connect()
+        {
+            return new NpgsqlConnection("Host=localhost;Username=postgre;Password=postgre;Database=mctg_db");
+        }
+    }
+}
